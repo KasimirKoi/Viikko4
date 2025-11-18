191a0ba3f26db19d90eae7b7f080edc95f1c3a68
KasimirKoi <kasimir.koistinen@centria.fi>
2025-11-18 13:31:12 +0200

    A osa push

---
+++ lue_varaukset.py
@@ -0,0 +1,80 @@
+"""
+Ohjelma joka tulostaa tiedostosta luettujen varausten alkiot ja niiden tietotyypit
+
+varausId | nimi | sähköposti | puhelin | varauksenPvm | varauksenKlo | varauksenKesto | hinta | varausVahvistettu | varattuTila | varausLuotu
+------------------------------------------------------------------------
+201 | Muumi Muumilaakso | muumi@valkoinenlaakso.org | 0509876543 | 2025-11-12 | 09:00 | 2 | 18.50 | True | Metsätila 1 | 2025-08-12 14:33:20
+int | str | str | str | datetime.date | datetime.time | int | float | bool | str | datetime
+------------------------------------------------------------------------
+202 | Niiskuneiti Muumilaakso | niisku@muumiglam.fi | 0451122334 | 2025-12-01 | 11:30 | 1 | 12.00 | False | Kukkahuone | 2025-09-03 09:12:48
+int | str | str | str | datetime.date | datetime.time | int | float | bool | str | datetime
+------------------------------------------------------------------------
+203 | Pikku Myy Myrsky | myy@pikkuraivo.net | 0415566778 | 2025-10-22 | 15:45 | 3 | 27.90 | True | Punainen Huone | 2025-07-29 18:05:11
+int | str | str | str | datetime.date | datetime.time | int | float | bool | str | datetime
+------------------------------------------------------------------------
+204 | Nipsu Rahapulainen | nipsu@rahahuolet.me | 0442233445 | 2025-09-18 | 13:00 | 4 | 39.95 | False | Varastotila N | 2025-08-01 10:59:02
+int | str | str | str | datetime.date | datetime.time | int | float | bool | str | datetime
+------------------------------------------------------------------------
+205 | Hemuli Kasvikerääjä | hemuli@kasvikeraily.club | 0463344556 | 2025-11-05 | 08:15 | 2 | 19.95 | True | Kasvitutkimuslabra | 2025-10-09 16:41:55
+int | str | str | str | datetime.date | datetime.time | int | float | bool | str | datetime
+------------------------------------------------------------------------
+"""
+from datetime import datetime
+
+def muunna_varaustiedot(varaus: list) -> list:
+    # Tähän tulee siis varaus oletustietotyypeillä (str)
+    # Varauksessa on 11 saraketta -> Lista -> Alkiot 0-10
+    # Muuta tietotyypit haluamallasi tavalla -> Seuraavassa esimerkki ensimmäisestä alkioista
+    muutettuvaraus = []
+    # Ensimmäisen alkion = varaus[0] muunnos
+    muutettuvaraus.append(int(varaus[0]))
+    # Ja tästä jatkuu
+    #nimi (str)
+    muutettuvaraus.append(varaus[1])
+    #sposti (str)
+    muutettuvaraus.append(varaus[2])
+    #puhelinnumero (str)
+    muutettuvaraus.append(varaus[3])
+    #varauspvm (datetime.date)
+    muutettuvaraus.append(datetime.strptime(varaus[4], "%Y-%m-%d").date())
+    #kellonaika (datetime.time)
+    muutettuvaraus.append(datetime.strptime(varaus[5], "%H:%M").time())
+    #kesto (int)
+    muutettuvaraus.append(int(varaus[6]))
+    #hinta (float)
+    muutettuvaraus.append(float(varaus[7]))
+    #varaus vahvistettu (bool)
+    muutettuvaraus.append(varaus[8] == "True")
+    #varattu tila (str)
+    muutettuvaraus.append(varaus[9])
+    #varaus luotu (datetime.datetime)
+    muutettuvaraus.append(datetime.strptime(varaus[10], "%Y-%m-%d %H:%M:%S"))
+    return muutettuvaraus
+
+def hae_varaukset(varaustiedosto: str) -> list:
+    # HUOM! Tälle funktioille ei tarvitse tehdä mitään!
+    # Jos muutat, kommentoi miksi muutit
+    varaukset = []
+    varaukset.append(["varausId", "nimi", "sähköposti", "puhelin", "varauksenPvm", "varauksenKlo", "varauksenKesto", "hinta", "varausVahvistettu", "varattuTila", "varausLuotu"])
+    with open(varaustiedosto, "r", encoding="utf-8") as f:
+        for varaus in f:
+            varaus = varaus.strip()
+            varaustiedot = varaus.split('|')
+            varaukset.append(muunna_varaustiedot(varaustiedot))
+    return varaukset
+
+def main():
+    # HUOM! seuraaville riveille ei tarvitse tehdä mitään!
+    # Jos muutat, kommentoi miksi muutit
+    # Kutsutaan funkioita hae_varaukset, joka palauttaa kaikki varaukset oikeilla tietotyypeillä
+    varaukset = hae_varaukset("varaukset.txt")
+    print(" | ".join(varaukset[0]))
+    print("------------------------------------------------------------------------")
+    for varaus in varaukset[1:]:
+        print(" | ".join(str(x) for x in varaus))
+        tietotyypit = [type(x).__name__ for x in varaus]
+        print(" | ".join(tietotyypit))
+        print("------------------------------------------------------------------------")
+
+if __name__ == "__main__":
+    main()
